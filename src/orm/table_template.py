03d39c66ead0d02e41d40b141fc7bb57e1c92520
Chad Estioco <chadestioco@gmail.com>
2014-05-14 01:14:45 +0800

    Added session making code.

--- src/orm/table_template.py
+++ src/orm/table_template.py
@@ -1,10 +1,14 @@
 from db_config import DBConfig
 
-from sqlalchemy import Column, Integer, String, TIMESTAMP
+from sqlalchemy import Column, Integer, String, TIMESTAMP, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 DATA_SOURCE = DB_DRIVER + "://" + DB_USERNAME + ":" + DB_PASSWORD + "@" + DB_HOST + "/" + DB_NAME 
+
+# Source http://docs.sqlalchemy.org/en/rel_0_9/orm/session.html
+db_engine = create_engine(DATA_SOURCE)
 
 class TableTemplate(Base):
     __tablename__ = None
