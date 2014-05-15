b867d541ace37f7fb5873d75f3e6ba4c605f935c
Chad Estioco <chadestioco@gmail.com>
2014-05-15 12:22:33 +0800

    Implementations from defuze.

--- src/orm/orm_base.py
+++ src/orm/orm_base.py
@@ -67,3 +67,13 @@ class SAEngine(plugins.SimplePlugin):
         self.sa_engine = None
         self.bus.subscribe("bind", self.bind)
 
+    def start(self):
+        self.sa_engine = create_engine(DBConfig.DSN)
+    
+    def stop(self):
+        if self.sa_engine:
+            self.sa_engine.dispose()
+            self.sa_engine = None
+
+    def bind(self, session):
+        session.configure(bind=self.sa_engine)
