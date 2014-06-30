1c6634cb81961f88e391ff313eb22d56c9cc00cd
Chad Estioco <chadestioco@gmail.com>
2014-06-30 18:51:48 +0800

    Restructuring login model so we can add userid to the session.

--- web/models/login.py
+++ web/models/login.py
@@ -3,6 +3,17 @@ from web.orm.mappings import Users
 import cherrypy
 
 def login_check(username, password):  
+    """
+    Returns the user id of the queried user if user exists; otherwise, returns
+    None.
+    """
     q =  cherrypy.request.db.query(Users).filter(Users.username==username, \
-      Users.password==password)
-    return cherrypy.request.db.query(q.exists())[0][0]
+      Users.password==password).limit(1)
+
+    
+    userdata = cherrypy.request.db.execute(q)
+    with userdata as data:
+        the_user = data.fetchone()
+
+        if the_user:
+            return the_user["userid"]
