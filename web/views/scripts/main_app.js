f4639979405b3d2ecea28839de31a970dab989e7
Chad Estioco <chadestioco@gmail.com>
2014-07-21 11:29:22 +0800

    More experiments with jQuery DataTables.
    Can't seem to get the tutorial to work, though.

--- web/views/scripts/main_app.js
+++ web/views/scripts/main_app.js
@@ -4,6 +4,11 @@ Javascript for main_app. Assumes that jQuery Datatables is loaded.
 @author Chad Estioco
 */
 
+var data = [
+    ["January 2014", "888", "0"],
+    ["February 2014", "888", "0"]
+    ]
+
 $(document).ready(function(){
-    $("#finance_summary").dataTable();
+    $("#finance_summary").DataTable({data: data});
 });
