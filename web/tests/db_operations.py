4d967e20c5a39961ac655706e7c9300084b33ef7
Chad Estioco <chadestioco@gmail.com>
2014-07-23 18:39:02 +0800

    Adding tests for db operations.
    Not yet working. Some notes.
     - Error on cherrypy.requests.db .
     - Tests seem to block even with cherrypy.engine.exit()

---
+++ web/tests/db_operations.py
@@ -0,0 +1,30 @@
+from web.models.operations import DBOperations
+from web.orm.mappings import Users
+from web.orm.orm_base import DBSessionTool, SAEngine
+
+import cherrypy
+import unittest
+
+class DBOperationsTest(unittest.TestCase):
+    
+    def insert_test(self):
+        pass
+
+    def test_select(self):
+        user = Users()
+        user.username = "hsimpson"
+        user.password = "donuts"
+        user_operations = DBOperations(user)
+        user_operations.insert()
+
+        user_selector = DBOperations(Users())
+        filter_fn = lambda x : User.username == "hsimpson"
+        user_selector.select(filter_fn)
+
+if __name__ == "__main__":
+    SAEngine(cherrypy.engine).subscribe()
+    cherrypy.tools.db = DBSessionTool()
+    cherrypy.tree.mount(None)
+    cherrypy.engine.start()
+    unittest.main()
+    cherrypy.engine.exit()
