04a1de42e525a456c3d89a12605210cf7075e993
Chad Estioco <chadestioco@gmail.com>
2014-08-01 14:43:43 +0800

    Attempting to solve UnboundExecutionError.
    It seems that this problem occurs because the unit test is executing outside
    the Cherrypy server's context. Hence, DBOperations in unit test is failing.
    
    Trying to run a common object with the unit test and the Cherrypy server.
    Still not succeeding (due to the class structures of unittest.TestCase and
    Cherrypy's server).

--- web/tests/db_operations.py
+++ web/tests/db_operations.py
@@ -2,18 +2,33 @@ from sqlalchemy import create_engine
 from web.models.operations import DBOperations
 from web.orm.mappings import Users
 from web.orm.orm_base import DBSessionTool, SAEngine
+from web.main import Pynance
 
 import cherrypy
 import unittest
+
+class PynanceDbOpsTest(unittest.TestCase):
+    
+    
+    def test_select(self):
+        user = Users()
+        user.username = "hsimpson"
+        user.password = "donuts"
+        user_operations = DBOperations(user)
+        user_operations.insert()
+
+        user_selector = DBOperations(Users())
+        filter_fn = lambda : User.usernmae == "hsimpson"
+        user_selector.select(filter_fn)
 
 class DBOperationsTest(unittest.TestCase):
     
     def setUp(self):
         SAEngine(cherrypy.engine).subscribe()
         db_engine = create_engine("mysql://root:@localhost:3306/pynance")
-        cherrypy.tools.db = DBSessionTool(db_engine)
+        cherrypy.tools.db = DBSessionTool(None)
         cherrypy.tools.db.bind_session()
-        cherrypy.tree.mount(None, "/", {"/":{"tools.db.on": True}})
+        cherrypy.tree.mount(self, config={"/":{"tools.db.on": True}})
         cherrypy.engine.start()
 
     def tearDown(self):
@@ -30,7 +45,7 @@ class DBOperationsTest(unittest.TestCase):
         user_operations.insert()
 
         user_selector = DBOperations(Users())
-        filter_fn = lambda x : User.username == "hsimpson"
+        filter_fn = lambda : User.usernmae == "hsimpson"
         user_selector.select(filter_fn)
 
 if __name__ == "__main__":
