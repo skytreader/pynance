b86ce92e295d43c7cf9693be083262fce64dac9d
Chad Estioco <chadestioco@gmail.com>
2014-08-05 18:18:12 +0800

    Experimenting with the form of our unit test classes.
    Instead of assigning global setup and teardown methods, we can just add the
    routines in the setUp and tearDown methods of our classes. However, this can
    make our unit tests take longer because setUp and tearDown is called at the
    beginning and the end of each unittest, respectively.

--- web/tests/shallow_tests.py
+++ web/tests/shallow_tests.py
@@ -9,6 +9,7 @@ Equivalent to
 https://bitbucket.org/Lawouach/cherrypy-recipes/src/50aff88dc4e24206518ec32e1c32af043f2729da/testing/unit/serverless/test.py?at=default
 """
 
+"""
 def setUpModule():
     cherrypy.tree.mount(Pynance(), "/")
     cherrypy.engine.start()
@@ -17,8 +18,16 @@ setup_module = setUpModule
 def tearDownModule():
     cherrypy.engine.exit()
 teardown_module = tearDownModule
+"""
 
 class ShallowTests(BaseCherryPyTestCase):
+    
+    def setUp(self):
+        cherrypy.tree.mount(Pynance(), "/")
+        cherrypy.engine.start()
+
+    def tearDown(self):
+        cherrypy.engine.exit()
     
     def test_index(self):
         response = self.request("/")
