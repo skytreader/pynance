a52fb85270587909851e1984481fb992f557cf0a
Chad Estioco <chadestioco@gmail.com>
2023-02-28 19:46:45 +0100

    Fucking hell at last it creates the tables!

---
+++ src/main/java/net/skytreader/model/InstallationConfig.java
@@ -0,0 +1,37 @@
+package net.skytreader.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="installation_config")
+public class InstallationConfig extends CreativeAwesomeModel{
+
+    @Column
+    private String key;
+    @Column
+    private String value;
+
+    public InstallationConfig(String key, String value) {
+        super();
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
