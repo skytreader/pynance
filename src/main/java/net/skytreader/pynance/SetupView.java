f42945d1668cbc7315f2756fce58dfea238c057b
Chad Estioco <chadestioco@gmail.com>
2023-06-24 02:20:16 +0200

    Some more UI Design explorations.
    However, I am saddened to report, that it seems the UI components that
    would be crucial for this project are behind a PRO subscription. :(
    
    Looks like I have to abandon this approach, again.

--- src/main/java/net/skytreader/pynance/SetupView.java
+++ src/main/java/net/skytreader/pynance/SetupView.java
@@ -1,15 +1,18 @@
 package net.skytreader.pynance;
 
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Header;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.router.Route;
 
 @Route("/setup")
 public class SetupView extends VerticalLayout {
 
     public SetupView(){
-        TextField monthlyBudget = new TextField("Monthly Budget");
-        monthlyBudget.setRequired(true);
+        add(new Header(new H1("Pynance Set-up")));
+        NumberField monthlyBudget = new NumberField("Monthly Budget");
+        monthlyBudget.setMin(0);
         add(monthlyBudget);
     }
 }
